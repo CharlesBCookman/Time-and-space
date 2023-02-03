6709e4e7480ac7476a768b2b3f9fc5f2f4c5951b
CharlesBCookman <CharlesBCookman@gmail.com>
2023-02-03 15:31:10 -0800

    confirming constructor is able to work with more than one input

--- __tests__/space-and-time.tests.js
+++ __tests__/space-and-time.tests.js
@@ -2,11 +2,18 @@ import {Planet} from './../src/space-and-time.js';
 
 describe('mercury variable made from Planet Creator', () => {
 
-  test('should make a object with stats on a planet', () => {
+  test('should make a mercury variable object with stats on a planet', () => {
     let mercury = new Planet("mercury", 87.969)
     expect(mercury.name).toEqual("mercury");
     expect(mercury.oneYearIs).toEqual("0.24");
   });
 });
 
+describe('venus', () => {
 
+  test('should make a mercury variable object with stats on a planet', () => {
+    let mercury = new Planet("venus", 224.7)
+    expect(mercury.name).toEqual("venus");
+    expect(mercury.oneYearIs).toEqual("0.62");
+  });
+});
